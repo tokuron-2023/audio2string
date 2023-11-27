6e59739140eea03e06a2a4961eb6dff3633a7c9f
y-takahashi <s19c1068aq@s.chibakoudai.jp>
2023-11-28 06:01:20 +0900

    add capture_img srv & update

--- camera_capture.py
+++ camera_capture.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 import cv2
 import rospy
-from std_msgs.msg import String
+from std_srvs.srv import SetBool
 
 class camera_capture_node():
     def __init__(self):
-       self.sub = rospy.Subscriber("/speech_to_text", String, self.callback)
-       self.text = ""
+        rospy.init_node("camera_capture")
+        rospy.Service("capture_img", SetBool, self.capture_srv)
+        # rospy.Service("start_nav", SetBool, self.capture_srv)
+        rospy.spin()
 
-
-    def callback(self, data):
-        self.text = data
+    def capture_srv(self, req):
+        if req.data:
+            self.capture()
+        return True
 
     def capture(self):
-        if len(self.text.data) > 0:
-            #select port number(0, 1, 2, ...)
-            cap = cv2.VideoCapture(0)
-            ret, frame = cap.read()
-            cv2.imwrite("test.png", frame)
-            cap.release()
+        # rospy.wait_for_service("start_nav")
+        rospy.wait_for_service("capture_img")
+        #select port nu)mber(0, 1, 2, ...)
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cv2.imwrite("test.png", frame)
+        cap.release()
+        print("caputure_img successfully")
+
+if __name__ == '__main__':
+    camera_capture_node()
